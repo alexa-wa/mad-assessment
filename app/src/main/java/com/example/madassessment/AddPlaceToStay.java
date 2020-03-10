066de5f609ef4f68b1641130686375e5ab9680f3
Aleksandrs Bogackins <4bogaa25@solent.ac.uk>
2020-03-10 18:01:22 +0000

    imp: POIadd activity

--- app/src/main/java/com/example/madassessment/AddPlaceToStay.java
+++ app/src/main/java/com/example/madassessment/AddPlaceToStay.java
@@ -3,8 +3,11 @@ package com.example.madassessment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class AddPlaceToStay extends AppCompatActivity {
+public class AddPlaceToStay extends AppCompatActivity implements View.OnClickListener {
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,7 +16,38 @@ public class AddPlaceToStay extends AppCompatActivity {
         Intent intent = new Intent();
         Bundle bundle = new Bundle();
 
+        Button btnGo = (Button) findViewById(R.id.btn1);
+        Button btnClear = (Button) findViewById(R.id.btn2);
+
+        btnGo.setOnClickListener(this);
+        btnClear.setOnClickListener(this);
+
         intent.putExtras(bundle);
         setResult(RESULT_OK, intent);
     }
+
+    @Override
+    public void onClick(View view) {
+
+        EditText poiName = (EditText)findViewById(R.id.poiName);
+        EditText poiType = (EditText)findViewById(R.id.poiType);
+        EditText poiPrice = (EditText)findViewById(R.id.poiPrice);
+
+        switch (view.getId()) {
+            case R.id.btn1:
+                System.out.println("To be implemented");
+                break;
+
+            case R.id.btn2:
+                poiName.setText(null);
+                poiType.setText(null);
+                poiPrice.setText(null);
+                break;
+
+            default:
+                break;
+
+        }
+    }
+
 }
--- app/src/main/java/com/example/madassessment/MainActivity.java
+++ app/src/main/java/com/example/madassessment/MainActivity.java
@@ -20,6 +20,7 @@ import android.location.LocationManager;
 import android.location.LocationListener;
 import android.location.Location;
 import android.content.Context;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements LocationListener {
