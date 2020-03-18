36f959c5597d70268a76cb8601f412dd90085035
alexa-wa <4bogaa25@solent.ac.uk>
2020-03-18 16:44:52 +0200

    imp: SaveWebTask poster script

--- app/src/main/java/com/example/madassessment/SaveWebTask.java
+++ app/src/main/java/com/example/madassessment/SaveWebTask.java
@@ -1,15 +1,24 @@
 package com.example.madassessment;
 
 import android.os.AsyncTask;
+import android.os.Environment;
 import android.util.Log;
+import android.widget.Toast;
+
+import com.example.madassessment.dataEntities.PointOfInterestEntity;
+
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.overlay.OverlayItem;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class SaveWebTask extends AsyncTask<Void, Void, String> {
     private static final String TAG = "MainActivity";
@@ -22,30 +31,54 @@ public class SaveWebTask extends AsyncTask<Void, Void, String> {
             URL connectionUrl = new URL("https://www.hikar.org/course/ws/add.php");
             httpURLConnection = (HttpURLConnection) connectionUrl.openConnection();
 
-            // Replace all test values with actual values
-            String postData = "username=user002&name=test&type=test&price=test&lat=test&lon=test&year=20";
-            httpURLConnection.setDoOutput(true);
-            httpURLConnection.setFixedLengthStreamingMode(postData.length());
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(Environment.getExternalStorageDirectory().getAbsolutePath() + "/records.csv"));
+                String magicLine = "";
 
-            OutputStream outputStream = null;
-            outputStream = httpURLConnection.getOutputStream();
-            outputStream.write(postData.getBytes());
+                ArrayList<PointOfInterestEntity> storesEntities = new ArrayList<>();
 
-            if (httpURLConnection.getResponseCode() == 200) {
-                InputStream inputStream = httpURLConnection.getInputStream();
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-
-                String allRecords = "", magicLine;
-
-                while ((magicLine = bufferedReader.readLine()) != null) {
-                    allRecords += magicLine;
-                    return allRecords;
+                while((magicLine = reader.readLine()) != null) {
+                    String components[] = magicLine.split(",");
+                    PointOfInterestEntity pointOfInterestEntity = new PointOfInterestEntity(components[0], components[1], Double.parseDouble(components[2]), Double.parseDouble(components[3]), Double.parseDouble(components[4]));
+                    storesEntities.add(pointOfInterestEntity);
                 }
-            } 
-            else {
-                return "HTTP ERROR: " + httpURLConnection.getResponseCode();
-            }
 
+                for(int i = 0; i < storesEntities.size(); i++) {
+                    String getPoiName = storesEntities.get(i).getName();
+                    String getPoiType = storesEntities.get(i).getType();
+
+                    Double getPoiPrice = storesEntities.get(i).getPrice();
+                    Double getPoiLatitude = storesEntities.get(i).getLatitude();
+                    Double getPoiLongitude = storesEntities.get(i).getLongitude();
+
+                    String postData = "username=user002&name=" + getPoiName + "&type=" + getPoiType + "&price=" + getPoiPrice  + "&lat=" + getPoiLatitude + "&lon=" + getPoiLongitude + "&year=20";
+
+                    httpURLConnection.setDoOutput(true);
+                    httpURLConnection.setFixedLengthStreamingMode(postData.length());
+
+                    OutputStream outputStream = null;
+                    outputStream = httpURLConnection.getOutputStream();
+                    outputStream.write(postData.getBytes());
+
+                    if (httpURLConnection.getResponseCode() == 200) {
+                        InputStream inputStream = httpURLConnection.getInputStream();
+                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+                        String allRecords = "", magicLine2;
+
+                        while ((magicLine2 = bufferedReader.readLine()) != null) {
+                            allRecords += magicLine2;
+                            return allRecords;
+                        }
+                    }
+                    else {
+                        return "HTTP ERROR: " + httpURLConnection.getResponseCode();
+                    }
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
         } 
         catch (IOException e) {
             return e.toString();
